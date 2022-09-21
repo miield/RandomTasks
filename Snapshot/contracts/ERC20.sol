520c0167571e6b81a6c025bffc7878506287137a
miield <aodasola95@gmail.com>
2022-09-21 22:53:47 +0100

    correction

--- Snapshot/contracts/ERC20.sol
+++ Snapshot/contracts/ERC20.sol
@@ -7,20 +7,16 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract OyinToken is ERC20Snapshot, Ownable {
 
-    using Arrays for uint256[];
-    using Counters for Counters.Counter;
-
-    Counters.Counter private _currentSnapshotId;
-
     constructor() ERC20("OYIN TOKEN", "OYIN") {
-        _mint(msg.sender, 100000);
+        _mint(msg.sender, 100000000000000000000);
     }
 
-    function snapshot() external onlyOwner returns (uint256) {
-        _currentSnapshotId.increment();
-        uint256 currentId = _getCurrentSnapshotId();
-        emit Snapshot(currentId);
-        return currentId;
+    function snapshot() public onlyOwner returns (uint256){
+        return _snapshot();
+    }
+
+    function checkSnapshot() public view returns (uint256) {
+        return _getCurrentSnapshotId();
     }
 
 }
--- Snapshot/test/test.ts
+++ Snapshot/test/test.ts
@@ -7,7 +7,8 @@ describe("OyinToken contract", function () {
 
   let oyinToken: Contract;
   let owner: SignerWithAddress;
-  // let id;
+  // let other: SignerWithAddress;
+  let id;
 
   before(async function() {
 
@@ -21,15 +22,17 @@ describe("OyinToken contract", function () {
     oyinToken = await OyinToken.connect(owner).deploy();
 
     await oyinToken.deployed();
-    // await oyinToken.connect(owner).snapshot();
     console.log("OyinToken is deployed to:", oyinToken.address);
   })
 
+  it("Snapshot should revert if not called by the owner", async function () {
+    const snapmeshot = await oyinToken.connect(owner).snapshot();
+    expect(snapmeshot).to.be.revertedWith("Ownable: caller is not the owner"); 
+  });
+
   it("Snapshot should snapshot", async function () {
     const snapmeshot = await oyinToken.connect(owner).snapshot();
-    const currentId = 1;
-    // const currentId = await oyinToken._getCurrentSnapshotId();
-    expect(currentId).to.equal(Number(1));
+
   });
 
 });
